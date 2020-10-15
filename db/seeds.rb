a382cd56a6df2e3f338a4bad7304c5e02bc8e06f
EC2 Default User <ec2-user@ip-172-31-45-252.ec2.internal>
2020-10-15 02:52:47 +0000

    users#show

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 
 fnames = ["佐藤", "鈴木","高橋","田中"]
 gnames = ["太郎","次郎","花子"]
-email = ["aaaa","bbbb","cccc","dddd","eeee","ffff","gggg","hhhh","iiii","jjjj"]
+email = ["aaaa","bbbb","cccc","dddd","eeee","ffff","gggg","hhhh","iiii","jjjj","kkkk","llll","mmmm","nnnn","oooo"]
 
-(0..9).each do |number|
+(0..14).each do |number|
   User.create(
     name: "#{fnames[number % 4]} #{gnames[number % 3]}",
     email: "#{email[number]}" + number.to_s + "@example.com", 
