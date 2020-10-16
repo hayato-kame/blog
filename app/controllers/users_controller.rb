77e74b9d14a86d64c01eb4ff0f0de55ff6d5824e
EC2 Default User <ec2-user@ip-172-31-45-252.ec2.internal>
2020-10-16 03:16:54 +0000

    user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,19 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      flash[:success] = 'ユーザを登録しました。'
+      redirect_to @user
+    else 
+      flash.now[:danger] = 'ユーザの登録に失敗しました。'
+      render :new
+    end 
   end
 
   def edit
@@ -21,4 +31,13 @@ class UsersController < ApplicationController
 
   def destroy
   end
+  
+  
+  private 
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end 
+  
+  
 end
