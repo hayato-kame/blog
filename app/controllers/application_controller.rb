ed9c86cac623abd7129ad41209c0648341a3804b
EC2 Default User <ec2-user@ip-172-31-45-252.ec2.internal>
2020-10-16 10:24:01 +0000

    user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  
+  # moduleをMix-in  取り込む logged_in?メソッドはこの一行が無いと使えない
+  include SessionsHelper
+  
+  private 
+  
+  def require_user_logged_in
+    unless logged_in?
+      redirect_to login_url
+    end 
+  end 
+  
 end
