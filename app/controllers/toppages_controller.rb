9df14b8104c148c7a8374b5d33d17e6d2f0fa5a0
EC2 Default User <ec2-user@ip-172-31-45-252.ec2.internal>
2020-10-21 08:40:34 +0000

    html title img

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -7,6 +7,7 @@ class ToppagesController < ApplicationController
     if logged_in?
       @entry = current_user.entries.build # form_with用 空のインスタンスを用意する
       @entries = current_user.entries.order(id: :desc).page(params[:page]) # 一覧表示用
+      @user = current_user
     end 
   end
 end
